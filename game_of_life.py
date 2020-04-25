d9fb4e1452a15e64c3963ded21b206a32179172a
jun-g <daemonfreaks@gmail.com>
2020-04-25 14:02:08 +0700

    Initial commit.

---
+++ game_of_life.py
@@ -0,0 +1,144 @@
+# coding: utf-8
+
+from __future__ import print_function
+import curses
+import random
+import sys
+import time
+
+
+class Cell(object):
+
+    def __init__(self, y, x, manager):
+        self.y = y
+        self.x = x
+        self.is_alive = False
+        self.next_alive = False
+        self.manager = manager
+
+    def live(self):
+        self.is_alive = self.next_alive
+
+    def next(self):
+        count = self.manager.get_count_of_around_alive_cell(self.y, self.x)
+        if not self.is_alive and count == 3:
+            self.next_alive = True
+        elif self.is_alive and 2 <= count <= 3:
+            self.next_alive = True
+        elif self.is_alive and count <= 1 or count >= 4:
+            self.next_alive = False
+
+    def __str__(self):
+        if self.is_alive:
+            return "0"
+        return "."
+
+
+class CellManager(object):
+
+    def __init__(self):
+        self.rows = []
+
+    def add_row(self):
+        self.rows.append([])
+
+    def add_cell(self, row_no, cell):
+        self.rows[row_no].append(cell)
+
+    def get_count_of_around_alive_cell(self, y, x):
+        alive_cell_count = 0
+        for i in (-1, 0, 1):
+            for j in (-1, 0, 1):
+                if i == 0 and j == 0:
+                    continue
+                cell = self._get_cell(y+i, x+j)
+                if cell is None:
+                    continue
+                if cell.is_alive:
+                    alive_cell_count += 1
+        return alive_cell_count
+
+    def next(self):
+        for row in self.rows:
+            for cell in row:
+                cell.next()
+
+    def live(self):
+        for row in self.rows:
+            for cell in row:
+                cell.live()
+
+    def get_cells_string(self):
+        v = []
+        for row in self.rows:
+            for cell in row:
+                v.append(str(cell))
+                v.append(" ")
+            v.append("\n")
+        return "".join(v)
+
+    def _get_cell(self, y, x):
+        if y < 0 or x < 0:
+            return None
+        try:
+            return self.rows[y][x]
+        except:
+            return None
+
+
+def main(cell_count):
+
+    manager = CellManager()
+    for i in range(cell_count):
+        manager.add_row()
+        for j in range(cell_count):
+            cell = Cell(i, j, manager)
+            cell.is_alive = bool(int(random.random() + 0.5))
+            manager.add_cell(i, cell)
+
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(1)
+    stdscr.nodelay(1)
+    prev_str = ""
+
+    min_speed = 1.0
+    max_speed = 0.1
+    curr_speed = 0.5
+    speed_step = 0.1
+
+    while True:
+
+        curr_str = manager.get_cells_string()
+        stdscr.addstr(0, 0, curr_str)
+        stdscr.refresh()
+
+        # カーソル上を押すと早くなる
+        # カーソル下を押すと遅くなる
+        c = stdscr.getch()
+        if c == curses.KEY_UP and curr_speed > max_speed:
+            curr_speed -= speed_step
+        elif c == curses.KEY_DOWN and curr_speed < min_speed:
+            curr_speed += speed_step
+        elif c == 113:
+            sys.exit(0)
+        time.sleep(curr_speed)
+
+        manager.next()
+        manager.live()
+
+        if curr_str == prev_str:
+            break
+        else:
+            prev_str = curr_str
+
+    curses.nocbreak()
+    stdscr.nodelay(0)
+    stdscr.keypad(0)
+    curses.echo()
+    curses.endwin()
+
+
+if __name__ == '__main__':
+    print(main(20))
