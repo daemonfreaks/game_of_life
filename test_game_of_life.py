015cccea3be322b85f8c25c81bde4d8e0ec059d7
Junji NAKANISHI <daemonfreaks@gmail.com>
2026-06-07 03:51:15 +0000

    test: add tests for get_count_of_around_alive_cell

--- test_game_of_life.py
+++ test_game_of_life.py
@@ -2,7 +2,7 @@
 An unit test module of game_of_life.py
 """
 
-from game_of_life import LifeCell
+from game_of_life import LifeCell, Universe
 
 
 def test_compute_next_state():
@@ -24,3 +24,25 @@ def test_compute_next_state():
     assert not cell.next_alive
     cell.compute_next_state(4)
     assert not cell.next_alive
+
+
+class TestUniverse(object):
+
+    def test_get_count_of_around_alive_cell(self):
+        universe = Universe()
+        universe.build_grid(3)
+
+        for i in range(3):
+            for j in range(3):
+                universe.rows[i][j].is_alive = True
+        assert universe.get_count_of_around_alive_cell(1, 1) == 8
+
+        for i in range(3):
+            for j in range(3):
+                universe.rows[i][j].is_alive = False
+        assert universe.get_count_of_around_alive_cell(1, 1) == 0
+
+        universe.rows[0][0].is_alive = True
+        universe.rows[1][0].is_alive = True
+        universe.rows[2][2].is_alive = True
+        assert universe.get_count_of_around_alive_cell(1, 1) == 3
